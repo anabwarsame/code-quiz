5d07f58a661d80468c0a07528cbe6940767c735a
anabwarsame <anab_1994@hotmail.co.uk>
2021-10-10 19:46:50 +0100

    add questions in js

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+const startButton = document.getElementById("start-quiz");
+
+startButton.addEventListener("click", startQuiz);
+
+function startQuiz() {
+    questionContainer
+}
+console.log("started");
+
+const capitalCityQuestions = [
+  {
+    question: "what is the capital city of portugal?",
+    answers: ["Madrid", "Paris", "Lisbon", " Athens"],
+    coorectAnswer: "Lisbon",
+  },
+  {
+    question: "what is the capital city of USA?",
+    answers: [" Washington DC", "New York", "Los Angeles", "Boston"],
+    coorectAnswer: "Washington DC",
+  },
+  {
+    question: "what is the capital city of Russia?",
+    answers: ["Moscow", "St Petersburg", "Prague", "Riga"],
+    coorectAnswer: "Moscow",
+  },
+  {
+    question: "what is the capita city of Canada?",
+    answers: ["Toronto", "Vancouver", "Ottawa", "Quebec"],
+    coorectAnswer: "Ottawa",
+  },
+];
+
+
+const currenQuestionIndex, shuffedQuestions
+const timer = setInterval(timerTick, 1000);
+
+startTimer;
