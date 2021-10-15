368a3a272d0e027ad785fb789442b55015e4f535
anabwarsame <anab_1994@hotmail.co.uk>
2021-10-15 12:16:33 +0100

    added functions to remove start button when click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,3 @@
-const startButton = document.getElementById("start-quiz");
-function renderQuestion()
-
-let currentQuestionIndex
-let shuffledQuestions
-const questionElement =document.createElement("h1")
-const answerButtons =document.createElement("button")
-
-
-startButton.addEventListener("click", startQuiz);
-
-function startQuiz() {
-  const questionContainer = document.createElement("div");
-  console.log("started");
-  startButton.remove();
-  shuffledQuestions= capitalCityQuestions.sort( ()=> Math.random()- .5)
-  currentQuestionIndex = 0
-  renderNextQuestion()
-}
- 
-function renderNextQuestion(){
-  displayQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function displayQuestion(question){
-  questionElement.innerText= question.question
-}
-
-
 const capitalCityQuestions = [
   {
     question: "what is the capital city of portugal?",
@@ -50,7 +21,80 @@ const capitalCityQuestions = [
   },
 ];
 
-// const currenQuestionIndex, shuffedQuestions
+const constructQuestionContainer = function (question) {
+  console.log(question);
+
+  // construct container div
+  const questionContainer = document.createElement("div");
+  questionContainer.setAttribute("class", "container question-container");
+  console.log(questionContainer);
+  // construct h2 element
+  const questionH2 = document.createElement("h2");
+  questionH2.setAttribute("class", "questions");
+  console.log(questionContainer);
+  // construct options div
+};
+
+//render question container
+const renderQuestionContainer = function () {
+  // get the current question, construct html for question container
+  // append the container to document
+  console.log("renderQuestionContainer");
+
+  const currentQuestion = capitalCityQuestions[0];
+
+  constructQuestionContainer(currentQuestion);
+};
+
+const removeStartContainer = function () {
+  console.log("removeStartContainer");
+  // remove start container from document
+  const startContainer = document.getElementById("start-quiz");
+  console.log(startContainer);
+  startContainer.remove();
+};
+
+// target start quiz button
+// add event listening to start quiz
+
+const startQuiz = function () {
+  console.log("start");
+  removeStartContainer();
+
+  renderQuestionContainer();
+};
+const startButton = document.getElementById("start-quiz");
+
+// add event listening to start quiz
+startButton.addEventListener("click", startQuiz);
+
+// remove start container
+
+// const startButton = document.getElementById("start-quiz");
+//function renderQuestion()
+
+//let currentQuestionIndex
+//let shuffledQuestions
+//const questionElement =document.createElement("h1")
+//const answerButtons =document.createElement("button")
+
+//startButton.addEventListener("click", startQuiz);
+
+//function startQuiz() {
+//const questionContainer = document.createElement("div");
+//console.log("started");
+//startButton.remove();
+//shuffledQuestions= capitalCityQuestions.sort( ()=> Math.random()- .5)
+//currentQuestionIndex = 0
+//renderNextQuestion()}
+
+//function renderNextQuestion(){
+//displayQuestion(shuffledQuestions[currentQuestionIndex]}
+
+//function displayQuestion(question){
+//questionElement.innerText= question.question}
+
+// const currentQuestionIndex, shuffledQuestions
 // const timer = setInterval(timerTick, 1000);
 
 //startTimer;
